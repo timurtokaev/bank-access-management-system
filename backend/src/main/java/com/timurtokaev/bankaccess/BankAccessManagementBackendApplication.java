b00adf126b62d4a2a53158a0260e3f3592097e28
Timur Tokayev <timurtokaev05@gmail.com>
2026-07-29 15:33:14 +0500

    feat: initialize Spring Boot backend

---
+++ backend/src/main/java/com/timurtokaev/bankaccess/BankAccessManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.timurtokaev.bankaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccessManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccessManagementBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/timurtokaev/bankaccess/BankAccessManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timurtokaev.bankaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccessManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
